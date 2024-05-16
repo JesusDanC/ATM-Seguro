e6d0941dd795427f15489c8c67ea1604dd413cdd
JesusDanC <107524073+JesusDanC@users.noreply.github.com>
2024-05-15 23:08:07 -0600

    Base del cliente con las librerias instaladas.

---
+++ client/src/main.js
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import PortalVue from 'portal-vue'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(VueAxios, axios)
+app.use(createPinia())
+app.use(PortalVue)
+
+app.mount('#app')
