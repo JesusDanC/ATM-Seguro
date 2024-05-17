3427fa8b5ee66c32886ecccf9672c090654bb6e2
JesusDanC <107524073+JesusDanC@users.noreply.github.com>
2024-05-16 20:35:29 -0600

    Se agrego Auth0

--- client/src/main.js
+++ client/src/main.js
@@ -5,11 +5,21 @@ import VueAxios from 'vue-axios'
 import PortalVue from 'portal-vue'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createAuth0 } from '@auth0/auth0-vue';
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(VueAxios, axios)
+app.use(
+    createAuth0({
+        domain: "{http://localhost:5173/}",
+        clientId: "{yourClientId}",
+        authorizationParams: {
+            redirect_uri: window.location.origin
+        }
+    })
+);  
 app.use(createPinia())
 app.use(PortalVue)
 
