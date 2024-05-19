f12ab907095d66d9d1ce559393c0edb97b3e1129
JesusDanC <107524073+JesusDanC@users.noreply.github.com>
2024-05-19 02:05:23 -0600

    Base login

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,47 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref, computed } from 'vue';
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <center><h1>Login</h1></center>
+    <form >
+      <input
+        type="text"
+        class="form-control my-2"
+        placeholder="nombre"
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      />
+      <input
+        type="text"
+        class="form-control my-2"
+        placeholder="pin"
+
+      />
+      <center><b-button type="submit">Acceder</b-button></center>
+    </form>
+    <div >
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
