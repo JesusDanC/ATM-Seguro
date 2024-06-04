f44f7e4922bf87c41b1747b3f3dddaf0fcfd1b60
JesusDanC <107524073+JesusDanC@users.noreply.github.com>
2024-06-04 01:08:59 -0600

    Frontend

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,57 +1,119 @@
-<script>
-export default {
-  data() {
-    return {
-      nombre: '',
-      pin: '',
-    };
-  },
-  methods: {
-    async login() {
-      try {
-        const response = await fetch('http://localhost:3000/api/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            nombre: this.nombre,
-            pin: this.pin,
-          }),
-        });
-        const data = await response.json();
-      } catch (error) {
-        console.error('Error al iniciar sesión:', error);
-      }
-    },
-  },
-};
+<script setup>
+  import { useRouter, useRoute, RouterLink, RouterView } from 'vue-router'
+  import { ref } from 'vue'
 
+  const opciones = ref([
+      {
+        nombre: "Home",
+        ruta: "/Home",
+        clase: "nav-link"
+      },
+      {
+        nombre: "Table",
+        ruta: "/Table",
+        clase: "nav-link"
+      },
+      {
+        nombre: "Form",
+        ruta: "/Form",
+        clase: "nav-link"
+      },
+      {
+        nombre: "Modal",
+        ruta: "/Modal",
+        clase: "nav-link"
+      },
+      {
+        nombre: "Usuarios",
+        ruta: "/Usuarios",
+        clase: "nav-link"
+      }
+    ]
+  )
+
+  const activar = (posicion) => {
+    for (let i = 0; i < opciones.value.length; i++) {
+      opciones.value[i].clase = "nav-link"
+    }
+    opciones.value[posicion].clase = "nav-link disabled"
+  }
+
+  const router = useRouter();
+  const route = useRoute()
+  const currentRoute = route
+
+  const ruta = () => {
+    for (let i = 0; i < opciones.value.length; i++) {
+      if (currentRoute.path === opciones.value[i].ruta) {
+        activar(i);
+      }else{
+        var Home = 0;
+        activar(Home);
+        router.push('/Home')
+      }
+    }
+  }
+  
+  window.onload = ruta;
 </script>
 
 <template>
-  <div class="container">
-    <center><h1>Login</h1></center>
-    <form >
-      <input
-        v-model="nombre"
-        type="text"
-        class="form-control my-2"
-        placeholder="nombre"
-
-      />
-      <input
-        v-model="pin"
-        type="text"
-        class="form-control my-2"
-        placeholder="pin"
-
-      />
-      <center><button @click.prevent="login">Iniciar sesión</button></center>
-    </form>
-  </div>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><RouterLink class="rainbow" to="/Home" @click="activar(0)">Tarea 6</RouterLink></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <template v-for="(opcion, index) in opciones">  
+            <li class="nav-item">
+              <a aria-current="page"><RouterLink :class="opcion.clase" :to="{path: opcion.ruta}" @click="activar(index)">
+                {{ opcion.nombre }}</RouterLink></a>
+            </li>
+          </template>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <RouterView />
 </template>
 
-<style>
+<style scoped>
 
+#shadowBox {
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.2);
+  border: 3px solid;
+}
+
+.rainbow {
+  text-decoration: none;
+  text-align: center;
+  animation: colorRotate 6s linear 0s infinite;
+}
+
+@keyframes colorRotate {
+  from {
+    color: #a34cde;
+  }
+  10% {
+    color: #3d0cc3bb;
+  }
+  35%{
+    color: #0c61c3bb;
+  }
+  50% {
+    color: #3396f9db;
+  }
+  65%{
+    color: #00fff7bb;
+  }
+  75% {
+    color: #109f96;
+  }
+  100% {
+    color:  #a34cde;
+  }
+}
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,17 +1,14 @@
 import './assets/main.css'
 
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import PortalVue from 'portal-vue'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
 
-app.use(VueAxios, axios)
-
 app.use(createPinia())
-app.use(PortalVue)
+app.use(router)
 
 app.mount('#app')
