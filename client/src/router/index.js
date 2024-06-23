7b5047c29587366e9588a898496e8b25a7b3933f
JesusDanC <107524073+JesusDanC@users.noreply.github.com>
2024-06-22 19:58:08 -0600

    Fix

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { useAuthStore } from '../stores/auth';
 import Login from '../views/Login.vue';
 import Registro from '../views/Registro.vue'
 import Home from '../views/Home.vue'
